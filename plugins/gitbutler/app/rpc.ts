528ee94bdb39d7abcb669f92498bdba0d72dafd5
Scott Sunarto <github@smsunarto.com>
2026-09-02 15:46:13 -0700

    chore(gitbutler): remove plugin

--- plugins/gitbutler/app/rpc.ts
+++
@@ -1,4 +0,0 @@
-import { createRPC } from "@bb-kit/core/rpc/query";
-import type plugin from "../server/server.ts";
-
-export const rpc = createRPC<(typeof plugin)["rpc"]>();
--- plugins/gitbutler/server/server.ts
+++
@@ -1,8 +0,0 @@
-import { definePlugin } from "@bb-kit/core/plugin";
-import { commitSelection } from "./rpc/commit-selection.ts";
-import { repository } from "./rpc/repository.ts";
-
-export default definePlugin({
-  pluginId: "gitbutler",
-  rpc: { repository, commitSelection },
-});
